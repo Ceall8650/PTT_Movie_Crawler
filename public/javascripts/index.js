8b98617c8a45e705e874931309e1c9636989aa42
leoshih <leoshih@qnap.com>
2019-03-29 21:52:52 +0800

    [Enhance] Add recommend rate and fix bugs

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -9,22 +9,35 @@ function search(e) {
             'content-type': 'application/json'
         },
         body: JSON.stringify(payload),
-    }).then(rs => rs.json())
-        .then(rs => {
-            const data = classify(rs);
-            showStats(data);
-            showArticls(data);
-        })
+    })
+    .then(rs => rs.json())
+    .then(rs => {
+        const data = classify(rs);
+        showStats(data);
+        showArticls(data);
+    })
 
 }
 
 function classify(data) {
-    const rs = {good: [], normal: [], bad: []};
+    const rs = {good: [], normal: [], bad: [], other: []};
 
     data.forEach(item => {
-        if(/好|爽|佳/.test(item.category)) rs.good.push(item);
-        else if(/無|普/.test(item.category)) rs.bad.push(item);
-        else rs.normal.push(item);
+        if(/好|爽|佳/.test(item.category)) {
+            rs.good.push(item);
+            return false;
+        }
+        else if(/爛|負/.test(item.category)) {
+            rs.bad.push(item);
+            return false;
+        }
+        else if(/普/.test(item.category)) {
+            rs.normal.push(item);
+            return false;
+        }
+        else {
+            rs.other.push(item);
+        }
     })
 
     return rs
@@ -34,11 +47,17 @@ function showStats(data) {
     const goodCnt = data.good.length;
     const nroamlCnt = data.normal.length;
     const badCnt = data.bad.length;
+    const otherCnt = data.other.length;
+    const total = goodCnt + badCnt + nroamlCnt;
 
-    document.querySelector('.total .num').innerHTML = goodCnt + badCnt + nroamlCnt;
+    document.querySelector('.total .num').innerHTML = total;
     document.querySelector('#stats .good .num').innerHTML = goodCnt;
     document.querySelector('#stats .normal .num').innerHTML = nroamlCnt;
     document.querySelector('#stats .bad .num').innerHTML = badCnt;
+    document.querySelector('#stats .other .num').innerHTML = badCnt;
+    document.querySelector('#result .temperature').innerHTML = `推薦指數: ${Math.round((goodCnt / total)*100)}％`;
+    document.querySelector('#result').classList.remove('hide');
+    if(total === 0) document.querySelector('#result').classList.add('hide');
 }
 
 function showArticls(data) {
@@ -69,7 +88,16 @@ function showArticls(data) {
             <td>${item.date}</td>
         </tr>
     `).join('')
+    otherList = data.other.map(item => `
+        <tr>
+            <td>${item.nrec}</td>
+            <td><a href="https://www.ptt.cc${item.link}" target="_blank">${item.title}</a></td>
+            <td>${item.author}</td>
+            <td>${item.date}</td>
+        </tr>
+    `).join('')
     document.querySelector('#list .good tbody').innerHTML = goodList;
     document.querySelector('#list .normal tbody').innerHTML = normalList;
     document.querySelector('#list .bad tbody').innerHTML = badList;
+    document.querySelector('#list .other tbody').innerHTML = otherList;
 }
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  res.render('index', { title: "PTT Movie Evaluation Query" });
+  res.render('index', { title: "鄉民溫度計 - PTT電影版" });
 });
 
 module.exports = router;
